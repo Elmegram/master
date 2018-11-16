03b9099446c87f1e417ad8ce88a710a1dc778cd5
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-11-16 19:15:44 +0100

    Write dummy bot

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,10 @@
 port module Main exposing (main)
 
+import Elmergram
+import Json.Decode as Decode
+import Json.Encode as Encode
 import Platform
+import Telegram
 
 
 main =
@@ -15,20 +19,51 @@ type alias Model =
     {}
 
 
+initModel =
+    {}
+
+
 init : () -> ( Model, Cmd Msg )
-init =
-    Debug.todo ""
+init _ =
+    ( initModel, Cmd.none )
 
 
 type Msg
-    = NoOp
+    = NewUpdate (Result Decode.Error Telegram.Update)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update =
-    Debug.todo ""
+update msg model =
+    case msg of
+        NewUpdate result ->
+            case result of
+                Err err ->
+                    ( model, Decode.errorToString err |> error )
+
+                Ok newUpdate ->
+                    handleUpdate model newUpdate
+
+
+handleUpdate : Model -> Telegram.Update -> ( Model, Cmd Msg )
+handleUpdate model newUpdate =
+    let
+        answer =
+            case newUpdate.content of
+                Telegram.MessageUpdate message ->
+                    Elmergram.answer "It works!" message.chat
+    in
+    ( model, Elmergram.encodeSendMessage answer |> sendMessage )
+
+
+port error : String -> Cmd msg
+
+
+port sendMessage : Encode.Value -> Cmd msg
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Debug.todo ""
+    incomingUpdate (Elmergram.decodeUpdate >> NewUpdate)
+
+
+port incomingUpdate : (Encode.Value -> msg) -> Sub msg
