a426f2d9f4d4bcf52c81d82ba70c2e309d03b6b5
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-11-25 21:05:29 +0100

    Externalize elmegram.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const Elmegram = require('./elmegram.js');
-const botPath = './bot.js';
+const Elmegram = require('elmegram.js');
+const Bot = require('./bot.js');
 
 const tokenName = 'TELEGRAM_TOKEN';
 const token = process.env[tokenName];
-Elmegram.startServer(token, botPath).catch(console.error);
+Elmegram.startServer(token, Bot).catch(console.error);
