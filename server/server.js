e55fa2afaf3abc4c3f24f10421bbf470b54e74f9
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-12-12 23:14:22 +0100

    Remove previous example

--- server/server.js
+++
@@ -1,7 +0,0 @@
-// Import Elmegram before bot, in order to fix Elm's dependency on Browser APIs.
-const Elmegram = require('elmegram.js');
-const Bot = require('./bot.js');
-
-const tokenName = 'TELEGRAM_TOKEN';
-const token = process.env[tokenName];
-Elmegram.startPolling(token, Bot).catch(console.error);
